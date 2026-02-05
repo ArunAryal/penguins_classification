9b5ead7e8c4b59cabc96eecb0c7363ca932803b8
ArunAryal <aryalashim55@gmail.com>
2026-02-05 19:53:22 +0545

    implemented pipeline and onehotencoding

--- deploy.py
+++ deploy.py
@@ -1,17 +1,20 @@
+import numpy as np
 import streamlit as st
 import joblib
 
 st.title("Penguin Classification")
 
-model=joblib.load("model.joblib")
+pipe=joblib.load("pipe.joblib")
 
+#user input
+island=st.text_input("Enter island")
 bill_length=st.number_input("Enter bill length(mm)")
 bill_depth=st.number_input("Enter bill depth(mm)")
 flipper_length=st.number_input("Enter flipper length(mm)")
 body_mass=st.number_input("Enter body mass(gram)")
 sex=st.text_input("Enter sex")
-sex_integer=1 if sex.lower()=='male' else 0
+X_user_input=np.array([island,bill_length,bill_depth,flipper_length,body_mass,sex],dtype=object).reshape(1,6)
 
 if(st.button("Predict")):
-    pred=model.predict([[bill_length,bill_depth,flipper_length,body_mass,sex_integer]])
+    pred=pipe.predict(X_user_input)
     st.success(f"Predicted species is{pred}")
